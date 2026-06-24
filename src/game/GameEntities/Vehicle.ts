373ed68f41179728795a1b5219f17cefbb5607bb
Isaiah Gardeen <igardeen@meditech.com>
2026-06-24 16:36:03 -0500

    started some physics math

---
+++ src/game/GameEntities/Vehicle.ts
@@ -0,0 +1,59 @@
+import { Vector2, Vector3 } from "three";
+
+export type HoverGroundVehicleStats = {
+    repulserThrustNewtons: number;
+    weightKg: number;
+}
+
+const cloneTankStats: HoverGroundVehicleStats = {
+    repulserThrustNewtons: 200_000,
+    weightKg: 50_000
+}
+
+export class HoverGroundVehicle {
+    stats: HoverGroundVehicleStats;
+    name: string;
+
+    private _inputVector: Vector2;
+
+    private _velocity: Vector3;
+
+    private _vehicleDirection: Vector3;
+
+    private _position: Vector3;
+
+    constructor(name: string, stats: HoverGroundVehicleStats, position?: Vector3) {
+        this.stats = stats;
+        this.name = name;
+        this._inputVector = new Vector2(0, 0);
+        this._velocity = new Vector3(0, 0, 0);
+        this._vehicleDirection = new Vector3(1, 0, 0);
+        this._position = position ? position : new Vector3(0, 0, 0);
+    }
+
+    setInput(input: Vector2) {
+        if (input.lengthSq() > 1) {
+            this._inputVector = input.clone().normalize();
+        } else {
+            this._inputVector = input;
+        }
+    }
+
+    setDirection(newDirection: Vector3) {
+        this._vehicleDirection = newDirection.clone().normalize();
+    }
+
+
+    simulateTick(tickLengthMs: number) {
+        const dt = tickLengthMs / 1000;
+        const inputThrust2D = this._inputVector.clone().multiplyScalar(this.stats.repulserThrustNewtons);
+        const inputThrust3D = new Vector3(inputThrust2D.x, 0, inputThrust2D.y);
+        const totalForces: Vector3 = inputThrust3D;
+        const acceleration = totalForces.divideScalar(this.stats.weightKg);
+        this._velocity.add(acceleration.multiplyScalar(dt));
+        this._position.add(this._velocity.clone().multiplyScalar(dt));
+    }
+
+
+
+}
--- src/game/utils.ts
+++ src/game/utils.ts
@@ -1,3 +1,5 @@
+
+
 export function clamp(value: number, min: number, max: number): number {
     return Math.min(Math.max(value, min), max);
 }
